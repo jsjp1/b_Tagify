3765851671f52e392e0dc9519d0a996249f2818a
JIEEEN <tjtpp009@korea.ac.kr>
2025-02-06 21:26:50 +0900

    mod model

--- server/app/models/tag.py
+++ server/app/models/tag.py
@@ -8,5 +8,5 @@ class Tag(Base):
   id = Column(Integer, primary_key=True, index=True)
   tagname = Column(String, unique=True, nullable=False, index=True)
   
-  user_tags = relationship("UserTag", back_populates="tag", cascade="all, delete-orphan")
-  video_tags = relationship("VideoTag", back_populates="tag", cascade="all, delete-orphan")
+  user_tag = relationship("UserTag", back_populates="tag", cascade="all, delete-orphan")
+  video_tag = relationship("VideoTag", back_populates="tag", cascade="all, delete-orphan")
