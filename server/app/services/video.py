06e72df4605f21ae0fe813cafe713a09485dfb63
JIEEEN <tjtpp009@korea.ac.kr>
2025-02-10 23:19:48 +0900

    mod video analyze

--- server/app/services/video.py
+++ server/app/services/video.py
@@ -12,7 +12,7 @@ from app.models.video_tag import VideoTag
 
 class VideoService():
   @staticmethod
-  def _get_caption(video_url: str, lang: str = 'ko') -> str:
+  def _extract_video_info(video_url: str, lang: str = 'ko') -> dict:
     """
     유튜브 비디오 링크 -> 자막 반환
     """
@@ -45,16 +45,40 @@ class VideoService():
                   captions.append(text)
 
       full_caption_text = " ".join(captions)
+      
+      video_info = {
+        "title": info.title,
+        "thumbnail": info.thumbnail,
+        "description": info.description,
+        "tags": info.tags,
+        "length": info.duration, # second
+        "summation": full_caption_text,
+      }
         
-      return full_caption_text
+      return video_info
     
   @staticmethod
   async def analyze_video(video: VideoAnalyze, db: Session, settings: Settings) -> VideoAnalyzeResponse:
     """
     유저 oauth id + 비디오 링크 + ... -> tag_count 만큼의 태그 리스트 생성, db 저장 후 video_id 반환
     """
-    caption = VideoService._get_caption(video.url)
-    print(caption)
+    video_info = VideoService._extract_video_info(video.url)
+    
+    # TODO: 만약 db에 존재하면 그거 가져와서 user에 추가, 아래 과정 X
+    
+    user_video = Video(
+      url = video.url,
+      title = video_info["title"],
+      thumbnail = video_info["thumbnail"],
+      summation = video_info["summation"],
+      video_length = video_info["length"],
+      user_id = video.oauth_id,
+    )
+        
+    db.add(user_video)
+    db.commit()
+    db.refresh(user_video)
+    
     return {"video_id": 123}
   
   @staticmethod
