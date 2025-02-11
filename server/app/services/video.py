4c565850ad2e7b241b558076c8d0df6e2c92735b
JIEEEN <tjtpp009@korea.ac.kr>
2025-02-11 21:32:49 +0900

    del get auto caption

--- server/app/services/video.py
+++ server/app/services/video.py
@@ -1,6 +1,5 @@
 from http.client import HTTPException
 from typing import List
-import requests
 from requests import Session
 from sqlalchemy.exc import IntegrityError
 import isodate
@@ -37,23 +36,6 @@ class VideoService():
         return 0
       
   @staticmethod
-  def _get_auto_captions(video_id: str, lang: str = "ko") -> str:
-      """
-      YouTube 자동 생성 자막 가져오기
-      """
-      caption_url = f"https://www.youtube.com/api/timedtext?v={video_id}&lang={lang}&fmt=srv3"
-      
-      response = requests.get(caption_url)
-      if response.status_code != 200:
-          return ""
-      
-      from xml.etree import ElementTree
-      root = ElementTree.fromstring(response.text)
-      captions = " ".join([elem.text for elem in root.findall(".//text") if elem.text])
-      
-      return captions
-    
-  @staticmethod
   def _extract_video_info(video_url: str, settings: Settings, lang: str = 'ko') -> dict:
     """
     유튜브 비디오 링크 -> 자막 반환
@@ -87,7 +69,7 @@ class VideoService():
         "description": snippet.get("description", ""),
         "tags": snippet.get("tags", []),
         "length": VideoService._convert_duration_to_seconds(content_details.get("duration", "")),
-        "summation": VideoService._get_auto_captions(video_id, lang),
+        "summation": "", # TODO: caption 관련 처리
     }
 
     return video_info
