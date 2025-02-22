7d2f3a2d23a36110fc707bda170923ccebeae81d
JIEEEN <tjtpp009@korea.ac.kr>
2025-02-22 20:06:48 +0900

    fix model_validate id

--- server/app/router/content.py
+++ server/app/router/content.py
@@ -54,7 +54,7 @@ async def save(
 ) -> ContentPostResponse:
     try:
         content_id = ContentService.post_content(content_type, request, db)
-        return ContentPostResponse.model_validate(id=content_id, from_attributes=True)
+        return ContentPostResponse.model_validate({"id": content_id}, from_attributes=True)
 
     except HTTPException as e:
         raise e
