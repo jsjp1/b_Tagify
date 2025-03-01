87004748cdd4ecc99dee097906092be6ed1c0c18
JIEEEN <tjtpp009@korea.ac.kr>
2025-03-01 20:36:46 +0900

    init model article, comment

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,4 +1,6 @@
+from app.models.article import Article
 from app.models.base import Base
+from app.models.comment import Comment
 from app.models.content_tag import content_tag_association
 from app.models.post_metadata import PostMetadata
 from app.models.tag import Tag
---
+++ server/app/models/article.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timezone
+
+from app.models.base import Base
+from sqlalchemy import BIGINT, Column, DateTime, Integer, String
+from sqlalchemy.orm import relationship
+
+
+class Article(Base):
+    __tablename__ = "articles"
+
+    id = Column(BIGINT, primary_key=True, index=True)
+    title = Column(String, nullable=False, default="")
+    body = Column(String, nullable=True, default="")
+    encoded_content = Column(String, nullable=False)
+    up_count = Column(Integer, default=0)
+    down_count = Column(Integer, default=0)
+
+    user_id = Column(BIGINT, ForeignKey("users.id", ondelete="CASCADE"))
+
+    created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
+    updated_at = Column(
+        DateTime,
+        default=datetime.now(timezone.utc),
+        onupdate=datetime.now(timezone.utc),
+        nullable=False,
+    )
+
+    user = relationship("User", back_populates="contents")
