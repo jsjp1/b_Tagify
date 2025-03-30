edd6cccf6a694eddb5afb9f0c366401b69460b26
JIEEEN <tjtpp009@korea.ac.kr>
2025-03-30 13:45:58 +0900

    mod get articles by tag limit order_by

--- server/app/services/article.py
+++ server/app/services/article.py
@@ -10,13 +10,8 @@ from app.models.content import Content
 from app.models.content_tag import content_tag_association
 from app.models.tag import Tag
 from app.models.user import User
-from app.schemas.article import (
-    AllArticlesLimitResponse,
-    ArticleCreate,
-    ArticleDelete,
-    ArticleDownload,
-    ArticleModel,
-)
+from app.schemas.article import (AllArticlesLimitResponse, ArticleCreate,
+                                 ArticleDelete, ArticleDownload, ArticleModel)
 from fastapi import HTTPException
 from sqlalchemy import and_, desc, func
 from sqlalchemy.exc import IntegrityError
@@ -353,6 +348,7 @@ class ArticleService:
             .filter(Article.tags.any(Tag.id == tag_id))
             .limit(limit)
             .offset(offset)
+            .order_by(desc(Article.updated_at))
             .all()
         )
 
