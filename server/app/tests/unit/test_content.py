f2f7623d1bcbe281827d23f0657d3ae711314818
JIEEEN <tjtpp009@korea.ac.kr>
2025-05-05 20:29:11 +0900

    fix typo

--- server/app/tests/unit/test_content.py
+++ server/app/tests/unit/test_content.py
@@ -2,278 +2,279 @@ import pytest
 from app.models.content import Content
 from sqlalchemy import select
 
-# @pytest.mark.asyncio
-# @pytest.mark.parametrize(
-#     "field",
-#     [
-#         "url",
-#         "title",
-#         "thumbnail",
-#         "favicon",
-#         "description",
-#         "video_length",
-#         "body",
-#         "tags",
-#     ],
-# )
-# @pytest.mark.parametrize(
-#     ("content_type", "body"),
-#     [
-#         (
-#             "post",
-#             {
-#                 "url": "https://www.github.com/",
-#                 "lang": "en",
-#                 "tag_count": 3,
-#                 "detail_degree": 3,
-#             },
-#         ),
-#         (
-#             "video",
-#             {
-#                 "url": "https://youtu.be/PCp2iXA1uLE?si=GvwmF0Wcoxe-mz7t",
-#                 "lang": "en",
-#                 "tag_count": 3,
-#                 "detail_degree": 3,
-#             },
-#         ),
-#     ],
-# )
-# async def test_analyze_success(
-#     auth_client, test_user_persist, content_type, body, field
-# ):
-#     """
-#     콘텐츠 분석 성공 시 200
-#     각 필드(url, title, thumbnail 등)가 응답에 포함되는지 확인
-#     """
-#     body["user_id"] = test_user_persist.id
 
-#     response = await auth_client.post(
-#         f"/api/contents/analyze?content_type={content_type}", json=body
-#     )
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "field",
+    [
+        "url",
+        "title",
+        "thumbnail",
+        "favicon",
+        "description",
+        "video_length",
+        "body",
+        "tags",
+    ],
+)
+@pytest.mark.parametrize(
+    ("content_type", "body"),
+    [
+        (
+            "post",
+            {
+                "url": "https://www.github.com/",
+                "lang": "en",
+                "tag_count": 3,
+                "detail_degree": 3,
+            },
+        ),
+        (
+            "video",
+            {
+                "url": "https://youtu.be/PCp2iXA1uLE?si=GvwmF0Wcoxe-mz7t",
+                "lang": "en",
+                "tag_count": 3,
+                "detail_degree": 3,
+            },
+        ),
+    ],
+)
+async def test_analyze_success(
+    auth_client, test_user_persist, content_type, body, field
+):
+    """
+    콘텐츠 분석 성공 시 200
+    각 필드(url, title, thumbnail 등)가 응답에 포함되는지 확인
+    """
+    body["user_id"] = test_user_persist.id
 
-#     assert response.status_code == 200
-#     assert field in response.json()
+    response = await auth_client.post(
+        f"/api/contents/analyze?content_type={content_type}", json=body
+    )
 
-
-# @pytest.mark.asyncio
-# @pytest.mark.parametrize(
-#     ("content_type", "body"),
-#     [
-#         (
-#             "post",
-#             {
-#                 "url": "https://www.invalidurl.com/",
-#                 "lang": "en",
-#                 "tag_count": 3,
-#                 "detail_degree": 3,
-#             },
-#         ),
-#         (
-#             "video",
-#             {
-#                 "url": "https://youtu.be/1234",
-#                 "lang": "en",
-#                 "tag_count": 3,
-#                 "detail_degree": 3,
-#             },
-#         ),
-#     ],
-# )
-# async def test_analyze_fail_with_invalid_url(
-#     auth_client, test_user_persist, content_type, body
-# ):
-#     """
-#     존재하지 않는 url analyze시 422 Unprocessable Entitiy
-#     """
-#     body["user_id"] = test_user_persist.id
-
-#     response = await auth_client.post(
-#         f"/api/contents/analyze?content_type={content_type}", json=body
-#     )
-
-#     if content_type == "post":
-#         # 해당 url이 존재하지 않음
-#         assert response.status_code == 422
-#     else:
-#         # 해당 video id의 video가 존재하지 않음
-#         assert response.status_code == 404
+    assert response.status_code == 200
+    assert field in response.json()
 
 
-# @pytest.mark.asyncio
-# @pytest.mark.parametrize(
-#     ("content_type", "body"),
-#     [
-#         (
-#             "post",
-#             {
-#                 "url": "https://www.github.com/",
-#                 "lang": "en",
-#                 "tag_count": 3,
-#                 "detail_degree": 3,
-#             },
-#         ),
-#     ],
-# )
-# async def test_analyze_fail_with_exists_content(
-#     auth_client, test_user_persist_with_content, content_type, body
-# ):
-#     """
-#     이미 존재하는 contents를 analyze하려할 때 400 Bad Request
-#     """
-#     body["user_id"] = test_user_persist_with_content.id
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("content_type", "body"),
+    [
+        (
+            "post",
+            {
+                "url": "https://www.invalidurl.com/",
+                "lang": "en",
+                "tag_count": 3,
+                "detail_degree": 3,
+            },
+        ),
+        (
+            "video",
+            {
+                "url": "https://youtu.be/1234",
+                "lang": "en",
+                "tag_count": 3,
+                "detail_degree": 3,
+            },
+        ),
+    ],
+)
+async def test_analyze_fail_with_invalid_url(
+    auth_client, test_user_persist, content_type, body
+):
+    """
+    존재하지 않는 url analyze시 422 Unprocessable Entitiy
+    """
+    body["user_id"] = test_user_persist.id
 
-#     response = await auth_client.post(
-#         f"/api/contents/analyze?content_type={content_type}", json=body
-#     )
+    response = await auth_client.post(
+        f"/api/contents/analyze?content_type={content_type}", json=body
+    )
 
-#     assert response.status_code == 400
-#     assert response.json()["detail"] == "Content already exists"
-
-
-# @pytest.mark.asyncio
-# @pytest.mark.parametrize("field", ["id", "tags"])
-# @pytest.mark.parametrize(
-#     ["content_type", "body"],
-#     [
-#         (
-#             "post",
-#             {
-#                 "url": "https://www.github.com/",
-#                 "title": "github",
-#                 "thumbnail": "",
-#                 "favicon": "",
-#                 "description": "",
-#                 "bookmark": False,
-#                 "video_length": 0,
-#                 "body": "",
-#                 "tags": ["temp"],
-#             },
-#         )
-#     ],
-# )
-# async def test_save_content_success(
-#     auth_client, test_user_persist, db_session, content_type, body, field
-# ):
-#     """
-#     새로운 콘텐츠를 저장할 때 200
-#     id, tags 필드가 포함되고 DB에 정상적으로 저장되는지 확인
-#     """
-#     body["user_id"] = test_user_persist.id
-
-#     # 저장 전엔 존재하지 않음을 확인
-#     async with db_session as session:
-#         result = await session.execute(
-#             select(Content).where(
-#                 and_(
-#                     Content.user_id == test_user_persist.id, Content.url == body["url"]
-#                 )
-#             )
-#         )
-#         db_content = result.scalar_one_or_none()
-#         assert db_content is None
-
-#     response = await auth_client.post(
-#         f"/api/contents/save?content_type={content_type}", json=body
-#     )
-
-#     response_json = response.json()
-
-#     # 저장 후에 존재함을 확인
-#     assert response.status_code == 200
-#     assert field in response_json
-#     async with db_session as session:
-#         result = await session.execute(
-#             select(Content).where(
-#                 and_(
-#                     Content.user_id == test_user_persist.id, Content.url == body["url"]
-#                 )
-#             )
-#         )
-#         db_content = result.scalar_one_or_none()
-#         assert db_content is not None
-#         assert db_content.user_id == test_user_persist.id
+    if content_type == "post":
+        # 해당 url이 존재하지 않음
+        assert response.status_code == 422
+    else:
+        # 해당 video id의 video가 존재하지 않음
+        assert response.status_code == 404
 
 
-# @pytest.mark.asyncio
-# @pytest.mark.parametrize(
-#     ["content_type", "body"],
-#     [
-#         (
-#             "post",
-#             {
-#                 "url": "https://www.github.com/",
-#                 "title": "github",
-#                 "thumbnail": "",
-#                 "favicon": "",
-#                 "description": "",
-#                 "bookmark": False,
-#                 "video_length": 0,
-#                 "body": "",
-#                 "tags": ["temp"],
-#             },
-#         )
-#     ],
-# )
-# async def test_save_content_fail_with_exists_content(
-#     auth_client, test_user_persist_with_content, content_type, body
-# ):
-#     """
-#     이미 저장된 콘텐츠를 다시 저장하려 하면 400 Bad Request
-#     'Content already exists' 메시지를 반환하는지 확인
-#     """
-#     body["user_id"] = test_user_persist_with_content.id
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("content_type", "body"),
+    [
+        (
+            "post",
+            {
+                "url": "https://www.github.com/",
+                "lang": "en",
+                "tag_count": 3,
+                "detail_degree": 3,
+            },
+        ),
+    ],
+)
+async def test_analyze_fail_with_exists_content(
+    auth_client, test_user_persist_with_content, content_type, body
+):
+    """
+    이미 존재하는 contents를 analyze하려할 때 400 Bad Request
+    """
+    body["user_id"] = test_user_persist_with_content.id
 
-#     response = await auth_client.post(
-#         f"/api/contents/save?content_type={content_type}", json=body
-#     )
+    response = await auth_client.post(
+        f"/api/contents/analyze?content_type={content_type}", json=body
+    )
 
-#     assert response.status_code == 400
-#     assert response.json()["detail"] == "Content already exists"
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Content already exists"
 
 
-# @pytest.mark.asyncio
-# async def test_delete_content_success(auth_client, test_user_persist_with_content, db_session):
-#     """
-#     콘텐츠 삭제 성공 -> 200
-#     'success' 메시지를 반환 및 DB에서 해당 콘텐츠가 실제로 삭제되었는지 확인
-#     """
-#     async with db_session as session:
-#         result = await session.execute(
-#           select(Content).where(Content.user_id == test_user_persist_with_content.id)
-#         )
-#         db_content = result.scalar_one_or_none()
-#         content_id = db_content.id
+@pytest.mark.asyncio
+@pytest.mark.parametrize("field", ["id", "tags"])
+@pytest.mark.parametrize(
+    ["content_type", "body"],
+    [
+        (
+            "post",
+            {
+                "url": "https://www.github.com/",
+                "title": "github",
+                "thumbnail": "",
+                "favicon": "",
+                "description": "",
+                "bookmark": False,
+                "video_length": 0,
+                "body": "",
+                "tags": ["temp"],
+            },
+        )
+    ],
+)
+async def test_save_content_success(
+    auth_client, test_user_persist, db_session, content_type, body, field
+):
+    """
+    새로운 콘텐츠를 저장할 때 200
+    id, tags 필드가 포함되고 DB에 정상적으로 저장되는지 확인
+    """
+    body["user_id"] = test_user_persist.id
 
-#     response = await auth_client.delete(
-#         f"/api/contents/{content_id}"
-#     )
+    # 저장 전엔 존재하지 않음을 확인
+    async with db_session as session:
+        result = await session.execute(
+            select(Content).where(
+                and_(
+                    Content.user_id == test_user_persist.id, Content.url == body["url"]
+                )
+            )
+        )
+        db_content = result.scalar_one_or_none()
+        assert db_content is None
 
-#     assert response.status_code == 200
-#     assert response.json()["message"] == "success"
+    response = await auth_client.post(
+        f"/api/contents/save?content_type={content_type}", json=body
+    )
 
-#     async with db_session as session:
-#         result = await session.execute(
-#             select(Content).where(Content.id == content_id)
-#         )
-#         db_content = result.scalar_one_or_none()
-#         assert not db_content
+    response_json = response.json()
+
+    # 저장 후에 존재함을 확인
+    assert response.status_code == 200
+    assert field in response_json
+    async with db_session as session:
+        result = await session.execute(
+            select(Content).where(
+                and_(
+                    Content.user_id == test_user_persist.id, Content.url == body["url"]
+                )
+            )
+        )
+        db_content = result.scalar_one_or_none()
+        assert db_content is not None
+        assert db_content.user_id == test_user_persist.id
 
 
-# @pytest.mark.asyncio
-# async def test_delete_content_fail(auth_client):
-#     """
-#     존재하지 않는 콘텐츠 ID 삭제 -> 404 Not Found
-#     'Content not found' 메시지를 반환하는지 확인
-#     """
-#     fake_content_id = 999999
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ["content_type", "body"],
+    [
+        (
+            "post",
+            {
+                "url": "https://www.github.com/",
+                "title": "github",
+                "thumbnail": "",
+                "favicon": "",
+                "description": "",
+                "bookmark": False,
+                "video_length": 0,
+                "body": "",
+                "tags": ["temp"],
+            },
+        )
+    ],
+)
+async def test_save_content_fail_with_exists_content(
+    auth_client, test_user_persist_with_content, content_type, body
+):
+    """
+    이미 저장된 콘텐츠를 다시 저장하려 하면 400 Bad Request
+    'Content already exists' 메시지를 반환하는지 확인
+    """
+    body["user_id"] = test_user_persist_with_content.id
 
-#     response = await auth_client.delete(
-#         f"/api/contents/{fake_content_id}"
-#     )
+    response = await auth_client.post(
+        f"/api/contents/save?content_type={content_type}", json=body
+    )
 
-#     assert response.status_code == 404
-#     assert response.json()["detail"] == "Content not found"
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Content already exists"
+
+
+@pytest.mark.asyncio
+async def test_delete_content_success(auth_client, test_user_persist_with_content, db_session):
+    """
+    콘텐츠 삭제 성공 -> 200
+    'success' 메시지를 반환 및 DB에서 해당 콘텐츠가 실제로 삭제되었는지 확인
+    """
+    async with db_session as session:
+        result = await session.execute(
+          select(Content).where(Content.user_id == test_user_persist_with_content.id)
+        )
+        db_content = result.scalar_one_or_none()
+        content_id = db_content.id
+
+    response = await auth_client.delete(
+        f"/api/contents/{content_id}"
+    )
+
+    assert response.status_code == 200
+    assert response.json()["message"] == "success"
+
+    async with db_session as session:
+        result = await session.execute(
+            select(Content).where(Content.id == content_id)
+        )
+        db_content = result.scalar_one_or_none()
+        assert not db_content
+
+
+@pytest.mark.asyncio
+async def test_delete_content_fail(auth_client):
+    """
+    존재하지 않는 콘텐츠 ID 삭제 -> 404 Not Found
+    'Content not found' 메시지를 반환하는지 확인
+    """
+    fake_content_id = 999999
+
+    response = await auth_client.delete(
+        f"/api/contents/{fake_content_id}"
+    )
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Content not found"
 
 
 @pytest.mark.asyncio
@@ -317,4 +318,5 @@ async def test_get_all_contents_success(
     assert response.status_code == 200
     assert isinstance(response_json, list)
     assert len(response_json) == content_len
+    assert field in response_json[0]
     assert field in response_json[0]
